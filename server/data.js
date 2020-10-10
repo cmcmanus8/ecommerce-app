ef79fa236770a9fecefdb1702ae87bbe7ff6d7c0
Calum McManus <calum@dovetaillab.com>
2020-10-10 19:56:47 +0100

    set up of client and server inc redux

---
+++ server/data.js
@@ -0,0 +1,44 @@
+export default {
+  products: [
+    {
+      _id: '1',
+      name: 'Slim Shirt',
+      category: 'Shirts',
+      image: '/images/d1.jpg',
+      price: 50,
+      brand: ' Nike',
+      rating: 4.5,
+      numReviews: 10
+    },
+    {
+      _id: '2',
+      name: 'Fit Shirt',
+      category: 'Shirts',
+      image: '/images/d1.jpg',
+      price: 50,
+      brand: ' Nike',
+      rating: 4.2,
+      numReviews: 10
+    },
+    {
+      _id: '3',
+      name: 'Slim Jeans',
+      category: 'Jeans',
+      image: '/images/d1.jpg',
+      price: 40,
+      brand: ' Nike',
+      rating: 4.6,
+      numReviews: 10
+    },
+    {
+      _id: '4',
+      name: 'Skinny Jeans',
+      category: 'Jeans',
+      image: '/images/d1.jpg',
+      price: 40,
+      brand: ' Nike',
+      rating: 4.7,
+      numReviews: 10
+    },
+  ]
+}
